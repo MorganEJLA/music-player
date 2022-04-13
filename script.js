4bf3a59782a204dc02016ebfa1949856abb11a46
Morgan J <40442819+MorganEJLA@users.noreply.github.com>
2022-04-13 14:35:03 -0700

    added functions for play and pause

---
+++ script.js
@@ -0,0 +1,28 @@
+const music = document.querySelector("audio");
+const prevBtn = document.querySelector("#prev");
+const playBtn = document.querySelector("#play");
+const nextBtn = document.querySelector("#next");
+// check if playing 
+let isPlaying = false;
+//Play
+
+function playSong(){
+    isPlaying=true;
+    playBtn.classList.replace("fa-play", "fa-pause");
+    playBtn.setAttribute("title", "Pause");
+    music.play();
+}
+
+//Pause
+
+function pauseSong(){
+    isPlaying = false;
+    playBtn.classList.replace("fa-pause", "fa-play");
+    playBtn.setAttribute("title", "Play");
+    music.pause();
+}
+
+// play or pause event listener //
+
+playBtn.addEventListener("click", () =>
+    (isPlaying ? pauseSong() : playSong()));
